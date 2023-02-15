75a135d4197708770bfd06b680d86a434d66d18c
zanzipanziby <zanzipanziby@gmail.com>
2023-02-15 14:03:58 +0300

    Commit and Push

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+          <h1>Commit and Push</h1>
          <img src={logo} className="App-logo" alt="logo" />
         <p>
           Hello World!
